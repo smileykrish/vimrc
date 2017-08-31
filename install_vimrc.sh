dbaa919fbd803f42d1204943efcfb6d3b4e906ec
knatesan <krishnamoorthy.natesan@us.fujitsu.com>
2017-08-31 04:42:19 -0500

    updated

--- install_vimrc.sh
+++ install_vimrc.sh
@@ -2,12 +2,15 @@
 set -e
 
 cd ~/.vim_config
+rm -f autoload/pathogen.vim
+cp sources_non_forked/vim-pathogen/autoload/pathogen.vim autoload/pathogen.vim
 
 echo 'set runtimepath+=~/.vim_config
 
 source ~/.vim_config/vimrcs/vimrc.vim
 source ~/.vim_config/vimrcs/filetypes.vim
 source ~/.vim_config/vimrcs/plugins_config.vim
+source ~/.vim_runtime/vimrcs/extended.vim
 
 try
 source ~/.vim_config/my_configs.vim
